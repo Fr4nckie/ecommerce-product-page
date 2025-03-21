019e678499248ae5b212d7a4004c043351956183
Franckie <franckie.dev@gmail.com>
2025-03-21 14:38:00 +0300

    feat: add CartContext

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import { createContext, useCallback, useContext, useState } from 'react'
+
+const CartContext = createContext({
+    product: null,
+    quantity: 1,
+    addProduct: () => {},
+    removeProduct: () => {}
+})
+
+export const useCart = () => {
+    return useContext(CartContext)
+}
+
+export const CartContextProvider = ({ children }) => {
+    const [product, setProduct] = useState(null)
+    const [quantity, setQuantity] = useState(1)
+
+    const addProduct = useCallback((newProduct) => {
+        setProduct(newProduct)
+    }, [])
+
+    const removeProduct = useCallback(() => {
+        setProduct(null)
+    }, [])
+
+    const incrementQuantity = useCallback(() => {
+        setQuantity(quantity + 1)
+    }, [quantity])
+
+    const decrementQuantity = useCallback(() => {
+        quantity > 1 ? setQuantity(quantity - 1) : null
+    }, [quantity])
+
+    return (
+        <CartContext.Provider
+            value={{
+                product,
+                quantity,
+                addProduct,
+                removeProduct,
+                incrementQuantity,
+                decrementQuantity
+            }}
+        >
+            {children}
+        </CartContext.Provider>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { GlobalStyles } from './styles/globalStyles.js'
+import { CartContextProvider } from './hooks/useCart.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <GlobalStyles />
-        <App />
+        <CartContextProvider>
+            <GlobalStyles />
+            <App />
+        </CartContextProvider>
     </StrictMode>
 )
